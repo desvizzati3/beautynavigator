0e07270fdacc1162a2f7754b060bfbb2ebbec87f
unknown <desvania.izzati@gmail.com>
2024-06-09 17:35:58 +0700

    perubahan front end bagian form

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,80 +1,94 @@
-<div class="max-w-md mx-auto">
-    <form action="/product" method="POST" enctype="multipart/form-data">
-        @csrf
+<x-app-layout>
+    <section class="min-h-screen bg-rose-50 py-8 ">
+        <div class="justify-center flex items-center mt-6 ">
+            <div class="bg-rose-100 w-2/3 shadow-md rounded-lg border-[5px] border-rose-800 p-6">
+                <h2 class="text-4xl font-bold font-abril mb-6 text-center">Tambah Produk</h2>
+                <form action="/product" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                        <!-- Kolom Pertama -->
+                        <div>
+                            <!-- Input Nama Produk -->
+                            <div class="mb-4">
+                                <label for="nama_produk" class="block text-sm font-medium text-gray-700">Nama Produk</label>
+                                <input type="text" name="nama_produk" id="nama_produk"
+                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    required>
+                            </div>
 
-        <!-- Input Nama Produk -->
-        <div class="mb-4">
-            <label for="nama_produk" class="block text-sm font-medium text-gray-700">Nama Produk</label>
-            <input type="text" name="nama_produk" id="nama_produk"
-                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                required>
-        </div>
+                            <!-- Input Nama Brand -->
+                            <div class="mb-4">
+                                <label for="brand" class="block text-sm font-medium text-gray-700">Brand</label>
+                                <input type="text" name="brand" id="brand"
+                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    required>
+                            </div>
 
-        <!-- Input Nama Brand -->
-        <div class="mb-4">
-            <label for="brand" class="block text-sm font-medium text-gray-700">Brand</label>
-            <input type="text" name="brand" id="brand"
-                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                required>
-        </div>
+                            <!-- Input Deskripsi -->
+                            <div class="mb-4">
+                                <label for="deskripsi" class="block text-sm font-medium text-gray-700">Deskripsi</label>
+                                <textarea name="deskripsi" id="deskripsi" rows="3"
+                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                    required></textarea>
+                            </div>
 
-        <!-- Input Deskripsi -->
-        <div class="mb-4">
-            <label for="deskripsi" class="block text-sm font-medium text-gray-700">Deskripsi</label>
-            <textarea name="deskripsi" id="deskripsi" rows="3"
-                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                required></textarea>
-        </div>
+                            <!-- Input Gambar -->
+                            <div class="mb-4">
+                                <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
+                                <input type="file" name="image" id="image"
+                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                            </div>
+                        </div>
 
-        <!-- Input Gambar -->
-        <div class="mb-4">
-            <label for="image" class="block text-sm font-medium text-gray-700">Image</label>
-            <input type="file" name="image" id="image"
-                class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-        </div>
+                        <!-- Kolom Kedua -->
+                        <div>
+                            <!-- Dropdown Skintone -->
+                            <div class="mb-4">
+                                <label for="skintone_id" class="block text-sm font-medium text-gray-700">Skintone</label>
+                                <select name="skintone_id" id="skintone_id"
+                                    class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required>
+                                    @foreach ($skintones as $skintone)
+                                        <option value="{{ $skintone->id }}">{{ $skintone->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-        <!-- Dropdown Skintone -->
-        <div class="mb-4">
-            <label for="skintone_id" class="block text-sm font-medium text-gray-700">Skintone</label>
-            <select name="skintone_id" id="skintone_id"
-                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required>
-                @foreach ($skintones as $skintone)
-                    <option value="{{ $skintone->id }}">{{ $skintone->nama }}</option>
-                @endforeach
-            </select>
-        </div>
+                            <!-- Dropdown Undertone -->
+                            <div class="mb-4">
+                                <label for="undertone_id" class="block text-sm font-medium text-gray-700">Undertone</label>
+                                <select name="undertone_id" id="undertone_id"
+                                    class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required>
+                                    @foreach ($undertones as $undertone)
+                                        <option value="{{ $undertone->id }}">{{ $undertone->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-        <!-- Dropdown Undertone -->
-        <div class="mb-4">
-            <label for="undertone_id" class="block text-sm font-medium text-gray-700">Undertone</label>
-            <select name="undertone_id" id="undertone_id"
-                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required>
-                @foreach ($undertones as $undertone)
-                    <option value="{{ $undertone->id }}">{{ $undertone->nama }}</option>
-                @endforeach
-            </select>
-        </div>
+                            <!-- Dropdown Category -->
+                            <div class="mb-4">
+                                <label for="category_id" class="block text-sm font-medium text-gray-700">Category</label>
+                                <select name="category_id" id="category_id"
+                                    class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required>
+                                    @foreach ($categories as $category)
+                                        <option value="{{ $category->id }}">{{ $category->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-        <!-- Dropdown Category -->
-        <div class="mb-4">
-            <label for="category_id" class="block text-sm font-medium text-gray-700">Category</label>
-            <select name="category_id" id="category_id"
-                class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->nama }}</option>
-                @endforeach
-            </select>
+                            <!-- Tombol Submit -->
+                            <div class="mt-6">
+                                <button type="submit"
+                                    class="inline-flex justify-center py-2 px-4 border-[2px] border-rose-200 shadow-sm text-sm font-medium rounded-full text-white bg-rose-800 hover:bg-rose-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-rose-300">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <!-- Tombol Submit -->
-        <div class="mt-4">
-            <button type="submit"
-                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                Submit
-            </button>
-        </div>
-    </form>
-</div>
+    </section>
+</x-app-layout>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot:title>{{ $title }}</x-slot>
-    <div class="bg-rose-50">
+    <div class="bg-rose-50 min-h-screen">
 
         {{-- button create --}}
         <center>
@@ -20,17 +20,21 @@
                                 <h3 class="text-lg font-semibold font-abril">{{ $product->nama_produk }}</h3>
                                 {{-- <p class="text-gray-600 font-abril">{{ $product->deskripsi }}</p> --}}
                                 <p class="text-gray-900 font-bold mt-2 font-abril">{{ $product->brand }}</p>
-
+                                <div class="flex gap-3">
                                 {{-- edit --}}
-                                <a href="/product/{{ $product->id }}/edit" class="badge bg-warning">Edit</a>
+                                <a href="/product/{{ $product->id }}/edit" class="badge bg-warning w-3 h-3">
+                                    <img src="https://img.icons8.com/?size=100&id=8192&format=png&color=000000" class="h-[22px] w-[22px]">
+                                </a>
 
                                 {{-- delete --}}
                                 <form action="/product/{{ $product->id }}" method="post" class="d-inline">
                                     @method('delete')
                                     @csrf
-                                    <button onclick="return confirm('Are you sure?')">Delete</button>
+                                    <button onclick="return confirm('Are you sure?')">
+                                        <img src="https://img.icons8.com/?size=100&id=102315&format=png&color=000000" alt="Delete" style="width: 24px; height: 24px;">
+                                    </button>
                                 </form>
-
+                                </div>
                             </div>
                         </a>
                     </article>
@@ -42,37 +46,3 @@
 
 
 
-<!--- yg lama
-    <x-app-layout>
-    {{-- <x-slot:title>{{ $title }}</x-slot> --}}
-    <section>
-        <div>
-            {{-- @foreach ($products as $product) --}}
-            <article class="py-8 max-w-screen-md border-b border-gray-300">
-                {{-- <a href="/products/{{ $product->id }}" class="hover:underline"> --}}
-                    {{-- <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $product['nama_produk'] }}</h2> --}}
-                    {{-- <img src="{{ $product->image }}" alt="{{ $product->nama_produk }}" class="w-full h-48 object-cover"> --}}
-                </a>
-                <div class="text-base">
-                    <a href="#" class="hover:underline text-gray-500">{{-- Brand:
-                         {{ $product['brand'] }}</a>
-                    |
-                    {{-- {{ $product->created_at->diffForHumans() }} --}}
-                    <br>
-                    {{-- <p class="text-green-700">Category: {{ $product->category->nama }}</p>
-                    <p class="text-pink-600">Undertone: {{ $product->undertone->nama }}</p>
-                    <p class="text-pink-600">Skintone: {{ $product->skintone->nama }}</p> --}}
-                </div>
-                {{-- <p class="my-4 font-light">{{ Str::limit($product['deskripsi'], 150) }}</p> --}}
-            </article>
-    {{-- @endforeach --}}
-        </div>
-    </section>
-    
-
-
-</x-app-layout>
-    
-    
-    
-    --->
